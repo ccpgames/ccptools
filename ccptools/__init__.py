939d09cc636f7f480c76a5d8ceae26662bbcdae5
ccp_zeulix <thordurm@ccpgames.com>
2024-04-04 15:31:27 +0000

    Version 1.0.0-rc.2 - Ready for the Wild...?
    - Let's see if this works!
    - Github Actions failed :P

--- ccptools/__init__.py
+++ ccptools/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '1.0.0-rc.1'
+__version__ = '1.0.0-rc.2'
 __author__ = 'Thordur Matthiasson <thordurm@ccpgames.com>'
 __license__ = 'MIT License'
 __copyright__ = 'Copyright 2013-2024 - CCP Games ehf'
